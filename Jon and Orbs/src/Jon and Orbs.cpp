9c3677a52f383a54857564aba2d3be0b65394472
ReaLNeroM <vlade.maksimovski@gmail.com>
2017-02-24 00:35:33 +0100

    Jon and Orbs

--- Jon and Orbs/src/Jon and Orbs.cpp
+++ Jon and Orbs/src/Jon and Orbs.cpp
@@ -3,41 +3,45 @@
 typedef long long ll;
 using namespace std;
 
-const int maxn = 1e6;
+const int maxn = 1e4;
 const double eps = 1e-11;
 int val[maxn];
+double dp[maxn];
+double achieved[maxn];
 int orbs, q;
-
-double calc(int adds){
-
-}
 
 int main(){
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 	cin >> orbs >> q;
+
+	dp[0] = 1.0;
+
+	for(int i = 1; i < maxn; i++){
+		for(int j = orbs - 1; j >= 0; j--){
+			double eempty = orbs - j;
+			eempty /= (double) orbs;
+			dp[j + 1] += eempty * dp[j];
+
+			eempty = j;
+			eempty /= (double) orbs;
+			dp[j] = dp[j] * eempty;
+		}
+		achieved[i] = dp[orbs];
+	}
+
 	for(int i = 0; i < q; i++){
 		double v;
 		cin >> v;
 		v -= eps;
 		v /= (double) 2000.0;
 
-		int best = 1e6;
-		int l = orbs, r = 1e6;
-		while(l <= r){
-			int mid = (l + r) / 2;
-
-			double get_chance = calc(mid);
-
-			if(v < get_chance){
-				r = mid - 1;
-				best = mid;
-			} else {
-				l = mid + 1;
+		for(int j = 1; j < maxn; j++){
+			if(achieved[j] > v){
+				cout << j << endl;
+				break;
 			}
 		}
-
-		cout << best << '\n';
 	}
 
 }
