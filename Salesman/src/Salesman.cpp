81fbb370a26a648f48dacfae14353002ac7a319e
ReaLNeroM <vlade.maksimovski@gmail.com>
2017-03-02 18:53:17 +0100

    Valley of Mexico and Salesman

--- Salesman/src/Salesman.cpp
+++ Salesman/src/Salesman.cpp
@@ -3,16 +3,144 @@
 typedef long long ll;
 using namespace std;
 
-const int maxn = 1e6;
-int val[maxn];
+const ll maxn = 5e5 + 5;
+static ll seg[2][4 * maxn];
+vector<pair<ll, ll>> fair[maxn];
+static ll used[maxn];
+static ll freel[maxn];
+static ll must[maxn];
+static ll reall[maxn];
+ll n, u, d, start;
+const ll INF = 2000000000;
+
+void spreadd(ll ind, ll l, ll r){
+	if(l != r){
+		ll mid = (l + r) / 2;
+
+		seg[0][2 * ind + 1] = max(seg[0][2 * ind + 1], seg[0][ind] - (r - mid) * d);
+		seg[0][2 * ind + 2] = max(seg[0][2 * ind + 2], seg[0][ind]);
+
+		seg[0][ind] = -INF;
+	}
+}
+
+void spreadu(ll ind, ll l, ll r){
+	if(l != r){
+		ll mid = (l + r) / 2 + 1;
+
+		seg[1][2 * ind + 1] = max(seg[1][2 * ind + 1], seg[1][ind]);
+		seg[1][2 * ind + 2] = max(seg[1][2 * ind + 2], seg[1][ind] - (mid - l) * u);
+
+		seg[1][ind] = -INF;
+	}
+}
+
+void do_low(ll pos, ll gotten, ll ind = 0, ll cl = 0, ll cr = maxn - 1){
+	if(pos < cl){
+		return;
+	} else if(cr <= pos){
+		spreadd(ind, cl, cr);
+		seg[0][ind] = max(seg[0][ind], gotten - (pos - cr) * d);
+		return;
+	}
+
+	ll mid = (cl + cr) / 2;
+
+	do_low(pos, gotten, 2 * ind + 1, cl     , mid);
+	do_low(pos, gotten, 2 * ind + 2, mid + 1, cr );
+}
+
+void do_up(ll pos, ll gotten, ll ind = 0, ll cl = 0, ll cr = maxn - 1){
+	if(pos <= cl){
+		spreadu(ind, cl, cr);
+		seg[1][ind] = max(seg[1][ind], gotten - (cl - pos) * u);
+		return;
+	} else if(cr < pos){
+		return;
+	}
+
+	ll mid = (cl + cr) / 2;
+
+	do_up(pos, gotten, 2 * ind + 1, cl     , mid);
+	do_up(pos, gotten, 2 * ind + 2, mid + 1, cr );
+}
+
+void upd(ll pos, ll gotten){
+	do_low(pos, gotten);
+	do_up(pos, gotten);
+}
+
+ll query(ll pos, ll ind = 0, ll l = 0, ll r = maxn - 1){
+	if(l == r){
+		return max(seg[0][ind], seg[1][ind]);
+	}
+	spreadd(ind, l, r);
+	spreadu(ind, l, r);
+
+	ll mid = (l + r) / 2;
+
+	if(pos <= mid){
+		return query(pos, 2 * ind + 1, l      , mid);
+	} else {
+		return query(pos, 2 * ind + 2, mid + 1, r  );
+	}
+}
 
 int main(){
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-	int n;
-	cin >> n;
+	cin >> n >> u >> d >> start;
+	start--;
 
+	for(ll i = 0; i < n; i++){
+		ll day;
+		cin >> day;
+		pair<ll, ll> curr_fair;
+		cin >> curr_fair.first >> curr_fair.second;
+		curr_fair.first--;
+		fair[day].push_back(curr_fair);
+	}
+
+	for(ll i = 0; i < 4 * maxn; i++){
+		seg[0][i] = seg[1][i] = -INF;
+	}
+
+	upd(start, 0);
+	for(ll i = 1; i < maxn; i++){
+		if(!fair[i].empty()){
+			sort(fair[i].begin(), fair[i].end());
+			for(ll j = 0; j < fair[i].size(); j++){
+				used[j] = query(fair[i][j].first);
+			}
+
+			ll size = fair[i].size();
+
+			must[0] = used[0] + fair[i][0].second;
+			freel[0] = fair[i][0].second;
+			for(ll j = 1; j < fair[i].size(); j++){
+				ll cost = (fair[i][j].first - fair[i][j - 1].first);
+				freel[j] = max(0LL, freel[j - 1] - (u + d) * cost) + fair[i][j].second;
+				must[j] = max(must[j - 1] - u * cost + fair[i][j].second, freel[j] + used[j]);
+			}
+
+			ll up_must = used[size - 1] + fair[i][size - 1].second;
+			ll up_freel = fair[i][size - 1].second;
+
+			reall[size - 1] = must[size - 1];
+			for(ll j = size - 2; j >= 0; j--){
+				ll cost = (fair[i][j + 1].first - fair[i][j].first);
+
+				up_freel = max(0LL, up_freel - cost * (u + d)) + fair[i][j].second;
+				up_must = max(up_must - cost * d + fair[i][j].second, used[j] + up_freel);
+
+				reall[j] = max(must[j] + up_freel, up_must + freel[j]) - fair[i][j].second;
+			}
+
+			for(ll j = 0; j < fair[i].size(); j++){
+				upd(fair[i][j].first, reall[j]);
+			}
+		}
+	}
+
+	cout << query(start);
 }
-
--- The Valley of Mexico/src/The Valley of Mexico.cpp
+++ The Valley of Mexico/src/The Valley of Mexico.cpp
@@ -3,16 +3,116 @@
 typedef long long ll;
 using namespace std;
 
-const int maxn = 1e6;
-int val[maxn];
+const int maxn = 1e3 + 2;
+int mat[maxn][maxn];
+bool seen[maxn][maxn][2][2];
 
 int main(){
 	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-	int n;
-	cin >> n;
+	int v, e;
+	cin >> v >> e;
 
+	for(int i = 0; i < e; i++){
+		int f, s;
+		cin >> f >> s;
+		f--, s--;
+
+		mat[f][s] = mat[s][f] = true;
+	}
+
+	for(int j = 0; j < v; j++){
+		for(int k = 0; k < v; k++){
+			for(int l = 0; l < 2; l++){
+				for(int m = 0; m < 2; m++){
+					seen[j][k][l][m] = false;
+					seen[2][k][l][m] = true;
+				}
+			}
+		}
+	}
+
+	for(int size = 3; size <= v; size++){
+		for(int i = 0; i < v; i++){
+			int l = i, r = (i + size - 1) % v;
+			int lnext = (i + 1) % v, rprev = (r + v - 1) % v;
+
+			if(mat[l][lnext] and seen[size - 1][lnext][1][0]){
+				seen[size][l][1][0] = true;
+			}
+			if(mat[r][rprev] and seen[size - 1][l][0][1]){
+				seen[size][l][0][1] = true;
+			}
+			if(mat[l][rprev] and seen[size - 1][l][0][1]){
+				seen[size][l][1][0] = true;
+			}
+			if(mat[lnext][r] and seen[size - 1][lnext][1][0]){
+				seen[size][l][0][1] = true;
+			}
+		}
+	}
+
+	for(int i = 0; i < v; i++){
+		int pos, k = 0, l = 0;
+		int start;
+		vector<int> in[v];
+		if(mat[i][(i + 1) % v] and seen[v][(i + 1) % v][1][0]){
+			pos = (i + 1) % v, k = 1, l = 0;
+			in[i].push_back((i + 1) % v);
+			in[(i + 1) % v].push_back(i);
+			start = i;
+		} else if(mat[(i + v - 1) % v][i] and seen[v][i][1][0]){
+			pos = i, k = 1, l = 0;
+			start = (i + v - 1) % v;
+			in[i].push_back((i + v - 1) % v);
+			in[(i + v - 1) % v].push_back(i);
+		} else {
+			continue;
+		}
+
+		for(int j = v; j >= 2; j--){
+			int size = j;
+			int cl = pos, cr = (pos + size - 1) % v;
+			int lnext = (pos + 1) % v, rprev = (cr + v - 1) % v;
+
+			if(mat[cl][lnext] and seen[size - 1][lnext][1][0] and k == 1){
+				in[cl].push_back(lnext);
+				in[lnext].push_back(cl);
+				pos = lnext, k = 1, l = 0;
+				continue;
+			}
+			if(mat[cr][rprev] and seen[size - 1][cl][0][1] and l == 1){
+				in[cr].push_back(rprev);
+				in[rprev].push_back(cr);
+				pos = cl, k = 0, l = 1;
+				continue;
+			}
+			if(mat[cl][rprev] and seen[size - 1][cl][0][1] and k == 1){
+				in[cl].push_back(rprev);
+				in[rprev].push_back(cl);
+				pos = cl, k = 0, l = 1;
+				continue;
+			}
+			if(mat[lnext][cr] and seen[size - 1][lnext][1][0] and l == 1){
+				in[lnext].push_back(cr);
+				in[cr].push_back(lnext);
+				pos = lnext, k = 1, l = 0;
+				continue;
+			}
+		}
+		pos = start;
+		for(int j = 0; j < v; j++){
+			cout << pos + 1 << endl;
+			int nnext = in[pos][0];
+			for(int k = 0; k < in[nnext].size(); k++){
+				if(in[nnext][k] == pos){
+					in[nnext].erase(in[nnext].begin() + k);
+					break;
+				}
+			}
+			pos = nnext;
+		}
+		return 0;
+	}
+	cout << -1;
 }
-
