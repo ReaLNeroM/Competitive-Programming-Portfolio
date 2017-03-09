173f731ae00f5da062d777a139b7a1a22959a5f8
ReaLNero <vlade.maksimovski@gmail.com>
2017-03-09 20:54:19 +0100

    Refactoring and BFFs for first subtask

--- BFFs/src/BFFs.cpp
+++ BFFs/src/BFFs.cpp
@@ -3,45 +3,71 @@
 using namespace std;
 typedef long long ll;
 
+int n;
+const int maxn = 1005;
+int bff[maxn];
+int place[maxn];
+int used[maxn];
+vector<int> adj[maxn];
+int res = 0;
+
+void dfs(int pos, int curr){
+	bool good = true;
+	for(int i = 0; i < curr; i++){
+		if(place[(i + curr - 1) % curr] == bff[place[i]]){
+		} else if(place[(i + 1) % curr] == bff[place[i]]){
+		} else {
+			good = false;
+		}
+	}
+	if(good){
+		res = max(res, curr);
+	}
+
+	if(bff[pos] == place[curr - 2]){
+		for(int i = 0; i < adj[i].size(); i++){
+			int nnext = adj[pos][i];
+			if(!used[nnext]){
+				used[nnext] = true;
+				place[curr] = nnext;
+				dfs(nnext, curr + 1);
+				used[nnext] = false;
+			}
+		}
+	} else if(!used[bff[pos]]){
+		used[bff[pos]] = true;
+		place[curr] = bff[pos];
+		dfs(bff[pos], curr + 1);
+		used[bff[pos]] = false;
+	}
+}
+
 int main(){
 	int t;
 	cin >> t;
 
 	for(int i = 0; i < t; i++){
-		int n;
 		cin >> n;
-		int bff[n];
+		for(int j = 0; j < n; j++){
+			bff[j] = -1;
+			place[j] = -1;
+		}
+		res = 0;	
+
 		for(int i = 0; i < n; i++){
 			cin >> bff[i];
 			bff[i]--;
+			adj[bff[i]].push_back(i);
 		}
 
-		int res = 0;
-
 		for(int i = 0; i < n; i++){
-			int used[n];
-			int place[n];
 			for(int j = 0; j < n; j++){
-				used[j] = -1;
+				used[j] = false;
 				place[j] = -1;
 			}
-			int pos = i;
-			cout << pos << ' ';
-			int found = 0;
-			int prev = -1;
-			while(used[pos] == -1){
-				used[pos] = found;
-				place[found] = pos;
-				found++;
-				prev = pos;
-				pos = bff[pos];
-				cout << pos << ' ';
-			}
-			cout << endl;
-
-			if(place[(found - 1 + found - 1) % found] == pos or place[0] == pos){
-				res = max(res, found);
-			}
+			used[i] = true;
+			place[0] = i;
+			dfs(i, 1);
 		}
 
 		cout << "Case #" << i + 1 << ": ";
