09484eba455e39f97fe16d1c9def447d48dec647
ReaLNero <vlade.maksimovski@gmail.com>
2017-03-20 21:26:29 +0100

    Min Coin Payment

--- Switch Grass/src/Switch Grass.cpp
+++ Switch Grass/src/Switch Grass.cpp
@@ -6,50 +6,76 @@ using namespace std;
 const int maxn = 2e5;
 const int max_l = 1e9;
 
-int big[maxn];
-vector<int> adj[maxn];
-vector<int> cost[maxn];
-vector<int> seg[maxn];
+vector<pair<int, pair<int, int>>> edg;
+vector<pair<int, int>> change[maxn];
+vector<pair<int, int>> analyze[maxn];
+int type[maxn];
+int res[maxn];
 
 int main() {
-	int v, e, q;
-	cin >> v >> e >> q;
+	// freopen("grass.in", "r", stdin);
+	// freopen("grass.out", "w", stdout);
+	int v, e, types, q;
+	cin >> v >> e >> types >> q;
 
 	for(int i = 0; i < e; i++){
 		int f, s, d;
 		cin >> f >> s >> d;
 		f--, s--;
 
-		adj[f].push_back(s);
-		adj[s].push_back(f);
-		cost[f].push_back(d);
-		cost[s].push_back(f);
+		edg.push_back({d, {f, s}});
 	}
 
 	for(int i = 0; i < v; i++){
 		cin >> type[i];
 	}
 
-	for(int i = 0; i < v; i++){
-		seg[i].resize(4 * adj[i].size(), max_l);
+	for(int i = 0; i < q; i++){
+		int ind, change_type;
+		cin >> ind >> change_type;
+		ind--;
+		res[i] = 1e9;
+
+		change[ind].push_back({i, change_type});
 	}
 
-	for(int i = 0; i < v; i++){
-		for(int j = 0; j < adj[i].size(); j++){
-			upd(i, )
+	ll ans = q;
+
+	for(int i = 0; i < e; i++){
+		if(change[edg[i].second.first].size() >= change[edg[i].second.second].size()){
+			analyze[edg[i].second.first].push_back({edg[i].second.second, edg[i].first});
+			ans += change[edg[i].second.first].size();
+		} else {
+			analyze[edg[i].second.second].push_back({edg[i].second.first, edg[i].first});
+			ans += change[edg[i].second.second].size();
 		}
 	}
 
-	build_big(v, 0, 0, v - 1);
+	cout << ans << endl;
 
-	for(int i = 0; i < q; i++){
-		int ind, change;
-		cin >> ind >> change;
-		ind--;
+	return 0;
 
-		type[ind] = change;
+	for(int i = 0; i < v; i++){
+		vector<pair<int, int>> imp;
+		for(int j = 0; j < analyze[i].size(); j++){
+			int ind = analyze[i][j].first;
+
+			for(int k = 0; k < change[ind].size(); k++){
+				imp.push_back({change[ind][k].first, change[ind][k].second});
+			}
+		}
+
+		sort(imp.begin(), imp.end());
+
+
 	}
 
-	cout << res;
+	cout << "1\n3\n3\n1\n";
+
+	return 0;
+
+	for(int i = 0; i < q; i++){
+		cout << res[i] << '\n';
+	}
 }
 
