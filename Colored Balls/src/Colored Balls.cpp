d6186b55555dca91ce0d34e576521d584c514aba
ReaLNero <vlade.maksimovski@gmail.com>
2017-03-30 20:01:05 +0200

    Work on a lot of tasks and Weird journey

---
+++ Colored Balls/src/Colored Balls.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const ll maxn = 505;
+ll val[maxn];
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+	ll n;
+	cin >> n;
+	for(int i = 0; i < n; i++){
+
+	}
+}
---
+++ JOIris/src/JOIris.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+const int maxn = 55;
+int val[maxn];
+
+int main(){
+	int n;
+	cin >> n;
+	int k;
+	cin >> k;
+
+	for(int i = 0; i < n; i++){
+		cin >> val[i];
+	}
+
+	queue
+}
