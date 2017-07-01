66edab2d5cea96d13e1eaed4fcfb1265309b2b85
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2017-07-01 20:48:37 +0200

    IOI 2017 Practice

--- Cup IOI 2017 Practice/grader.cpp
+++ Cup IOI 2017 Practice/grader.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <string>
 #include <cstdlib>
+#include <iostream>
 #include <cassert>
 using namespace std;
 
@@ -30,14 +31,17 @@ int main() {
     a.resize(tests);
     b.resize(tests);
     qc.resize(tests);
+    std::srand(60);
+
     for (t = 0; t < tests; t++) {
-    	// a[t] = rand() % 500000000;
-    	// b[t] = rand() % 500000000;
-    	// a[t] -= 5e8;
-    	// b[t] -= 5e8;
-        assert(2 == scanf("%d %d", &a[t], &b[t]));
+        a[t] = rand() % 1000000001;
+        b[t] = rand() % 1000000001;
+        a[t] -= 500000000;
+        b[t] -= 500000000;
+        // assert(2 == scanf("%d %d", &a[t], &b[t]));
     }
     for (t = 0; t < tests; t++) {
+        // std::cout << a[t] << ' ' << b[t] << std::endl;
         qc[t] = 0;
         vector<int> result = find_cup();
         if (int(result.size()) != 2) {
@@ -48,9 +52,10 @@ int main() {
         if (result[0] != x || result[1] != y) {
             qc[t] = -1;
         }
+        // std::cout << result[0] << ' ' << result[1] << ' ';
+        std::cout << qc[t] << std::endl;
     }
     for (t = 0; t < tests; t++) {
-        printf("%d\n", qc[t]);
     }
     return 0;
 }
