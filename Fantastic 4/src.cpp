432debb672d274fb551c15cf728a98df7168285e
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2017-07-13 12:46:50 +0200

    Fantastic 4

--- Fantastic 4/src.cpp
+++ Fantastic 4/src.cpp
@@ -8,54 +8,44 @@ int main(){
 	std::vector<ll> v(4);    
 
 	std::cin >> v[0] >> v[1] >> v[2] >> v[3];	
-	std::sort(v.begin(), v.end());
+    
+	ll most = 0;
 
-	// boost v[3];
-	ll small = v[0];
+    for(ll i = 3; i < 4; i++){
+    	auto v2 = v;
+    	std::swap(v2[i], v2[0]);
 
-	v[0] -= small;
-	v[1] -= small;
-	v[2] -= small;
-	v[3] += 2 * small;
+        for(ll j = 1; j < 4; j++){
+        	if(v2[j] < v2[1]){
+        		std::swap(v2[j], v2[1]);
+        	}
+        }
 
-	// boost v[0] then v[3] then v[3]
-	small = v[1] / 3;
+        std::cerr << v2[0] << ' ' << v2[1] << ' ' << v2[2] << ' ' << v2[3] << std::endl;
+        v2[0] += v2[1] * 2;
+        v2[2] -= v2[1];
+        v2[3] -= v2[1];
+        v2[1] -= v2[1];
+        std::cerr << v2[0] << ' ' << v2[1] << ' ' << v2[2] << ' ' << v2[3] << std::endl;
 
-	v[0] -= small * 2 - small * 2;
-	v[1] -= small * 3;
-	v[2] -= small * 3;
-	v[3] += small * 3;
+        ll x = std::min(v2[2], v2[3]) / 3;
 
-	ll most = v[3];
+        v2[0] += x * 3;
+        v2[1] += x * 2 - x * 2;
+        v2[2] -= x * 3;
+        v2[3] -= x * 3;
+        std::cerr << v2[0] << ' ' << v2[1] << ' ' << v2[2] << ' ' << v2[3] << std::endl;
+        
+        if(std::min(v2[2], v2[3]) == 2){
+        	v2[0]++;
+        	v2[1]++;
+        	v2[2] -= 2;
+        	v2[3] -= 2;
+        } 
+        std::cerr << v2[0] << ' ' << v2[1] << ' ' << v2[2] << ' ' << v2[3] << std::endl;
 
-	int steps = 0;
-	while(true){
-		std::sort(v.begin(), v.end());
-
-		if(v[0] == 0 and v[1] == 0){
-			break;
-		} else if(v[0] != 0){
-			v[0]--;
-			v[1]--;
-			v[2]--;
-			v[3] += 2;
-		} else {
-			v[0] += 2;
-			v[1]--;
-			v[2]--;
-			v[3]--;
-		}
-
-		if(steps > 1000){
-			break;
-		}
-		
-		steps++;
-		if(v[3] > most){
-			steps = 0;
-			most = v[3];
-		}
-	}
-
+    	most = std::max(most, v2[0]);
+    }
+    
 	std::cout << most << std::endl;
 }
