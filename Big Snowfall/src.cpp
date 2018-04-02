c2fd7da49b86eb1be6f17dac0a153faa998de298
ReaLNeroM <vlade.maksimovski@gmail.com>
2018-04-02 23:31:20 +0200

    After much work, Big Snowfall

--- Big Snowfall/src.cpp
+++ Big Snowfall/src.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <deque>
+#include <algorithm>
 #include <queue>
 
-const int maxn = 150;
+const int maxn = 2e5 + 1e2;
 int in[maxn];
 int out[maxn];
 int tagged[maxn];
@@ -10,8 +11,6 @@ std::deque<int> bi_e[maxn];
 int vertices, edges;
 
 int get_nonzero_components(){
-	std::fill(tagged, tagged + vertices, false);
-
 	int nonzero_components = 0;
 
 	for(int i = 0; i < vertices; i++){
@@ -40,6 +39,43 @@ int get_nonzero_components(){
 	return nonzero_components;
 }
 
+bool one_check(int nonzero_components){
+	bool one_problem_encountered = false;
+	int one_more_in = 0;
+	int one_more_out = 0;
+
+	for(int i = 0; i < vertices; i++){
+		if(in[i] == out[i] + 1){
+			one_more_in++;
+		} else if(in[i] == out[i] - 1){
+			one_more_out++;
+		} else if(in[i] != out[i]){
+			one_problem_encountered = true;
+		}
+	}
+
+	if(one_more_in <= 1 and one_more_out <= 1 and !one_problem_encountered and nonzero_components <= 1){
+		return true;
+	}
+
+	return false;
+}
+
+bool bi_check(int nonzero_components){
+	int bi_odd_degree = 0;
+	for(int i = 0; i < vertices; i++){
+		if(bi_e[i].size() % 2 == 1){
+			bi_odd_degree++;
+		}
+	}
+
+	if((bi_odd_degree == 0 or bi_odd_degree == 2) and nonzero_components <= 1){
+		return true;
+	}
+
+	return false;
+}
+
 int main() {
 	int t;
 	std::cin >> t;
@@ -50,6 +86,7 @@ int main() {
 		std::fill(in, in + vertices, 0);
 		std::fill(out, out + vertices, 0);
 		std::fill(tagged, tagged + vertices, 0);
+
 		for(int i = 0; i < vertices; i++){
 			bi_e[i].clear();
 		}
@@ -66,42 +103,9 @@ int main() {
 
 		int nonzero_components = get_nonzero_components();
 
-		bool one_check = false;
-
-		bool one_problem_encountered = false;
-		int one_more_in = 0;
-		int one_more_out = 0;
-
-		for(int i = 0; i < vertices; i++){
-			if(in[i] == out[i] + 1){
-				one_more_in++;
-			} else if(in[i] == out[i] - 1){
-				one_more_out++;
-			} else if(in[i] != out[i]){
-				one_problem_encountered = true;
-			}
-		}
-
-		if(one_more_in <= 1 and one_more_out <= 1 and !one_problem_encountered and nonzero_components == 1){
-			one_check = true;
-		}
-
-		int bi_check = false;
-
-		int bi_odd_degree = 0;
-		for(int i = 0; i < vertices; i++){
-			if(bi_e[i].size() % 2 == 1){
-				bi_odd_degree++;
-			}
-		}
-
-		if((bi_odd_degree == 0 or bi_odd_degree == 2) and nonzero_components == 1){
-			bi_check = true;
-		}
-
-		if(one_check){
+		if(one_check(nonzero_components)){
 			std::cout << "YES\n";
-		} else if(bi_check){
+		} else if(bi_check(nonzero_components)){
 			std::cout << "TRAFFIC STOPPING NEEDED\n";
 		} else {
 			std::cout << "WAKE UP EARLIER\n";
