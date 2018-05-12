d4e325ebeb875dbef18e20296cd243b623c2eb96
unknown <vlade.maksimovski@gmail.com>
2018-05-12 01:24:16 +0100

    Work on Dripping Water

---
+++ Dripping Water/grader.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include "src.cpp"
+
+typedef long long ll;
+
+int main(){
+	std::ios::sync_with_stdio(false);
+
+	DrippingWater x;
+	std::cout << x.fixCeiling({"xxoxx","oxoxo","xxoxx","xxoxx","xxoxx"}) << '\n';
+}
---
+++ Dripping Water/src.cpp
@@ -0,0 +1,117 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+int const ay[] = {1, -1, 0, 0};
+int const ax[] = {0, 0, 1, -1};
+
+class DrippingWater {
+	static const int maxn = 110;
+	static const int INF = 10001;
+	int source = 105;
+	int sink = 106;
+	int adj[maxn][maxn];
+	bool visited[maxn];
+	int par[maxn];
+	int n, y, x;
+
+	int flow(){
+		int res = 0;
+
+		bool gathered = true;
+		do {
+			gathered = false;
+
+			std::fill(visited, visited + maxn, false);
+			std::fill(par, par + maxn, -1);
+
+			std::queue<int> q;
+
+			q.push(source);
+			visited[source] = true;
+			par[source] = -2;
+
+			while(!q.empty() and !visited[sink]){
+				int fr = q.front();
+				q.pop();
+
+				for(int i = 0; i < maxn; i++){
+					if(adj[fr][i] and !visited[i]){
+						q.push(i);
+						visited[i] = true;
+						par[i] = fr;
+					}
+				}
+			}
+
+			if(!visited[sink]){
+				break;
+			}
+
+			int pos = sink;
+
+			int flow_found = INF;
+			while(pos != source and par[pos] != -1){
+				flow_found = std::min(flow_found, adj[par[pos]][pos]);
+				pos = par[pos];
+			}
+
+			res += flow_found;
+			gathered = true;
+
+			pos = sink;
+
+			while(pos != source){
+				adj[par[pos]][pos] -= flow_found;
+				adj[pos][par[pos]] += flow_found;
+				pos = par[pos];
+			}
+		} while(gathered);
+
+		return res;
+	}
+
+	int ind(int a, int b, int c){
+		return a * x * 2 + b * 2 + c;
+	}
+
+public:
+	int fixCeiling(std::vector<std::string> ceilMap){
+		y = ceilMap.size();
+		x = ceilMap[0].size();
+
+		n = y * x;
+
+		for(int j = 0; j < x; j++){
+			if(ceilMap[0][j] == 'x'){
+				adj[source][ind(0, j, 0)]++;
+			}
+			if(ceilMap[y - 1][j] == 'x'){
+				adj[ind(y - 1, j, 1)][sink]++;
+			}
+		}
+
+		for(int i = 0; i < y; i++){
+			for(int j = 0; j < x; j++){
+				if(ceilMap[i][j] == 'x'){
+					adj[ind(i, j, 0)][ind(i, j, 1)] += 1;
+				}
+			}
+		}
+
+		for(int i = 0; i < y - 1; i++){
+			for(int j = 0; j < x; j++){
+				for(int k = 0; k < 4; k++){
+					int ni = i + ay[k];
+					int nj = j + ax[k];
+
+					if(0 <= ni and ni < y and 0 <= nj and nj < x and ceilMap[ni][nj] == 'x'){
+						adj[ind(i, j, 1)][ind(ni, nj, 0)] += INF;
+					}
+				}
+			}
+		}
+
+		return flow();
+	}
+};
