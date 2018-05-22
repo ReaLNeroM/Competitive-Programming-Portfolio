ed273786e369dc9222673bd8240c511e7d6ce7bf
unknown <vlade.maksimovski@gmail.com>
2018-05-22 02:39:15 +0200

    Some refactoring

--- Billiard/src.cpp
+++ Billiard/src.cpp
@@ -2,20 +2,16 @@
 
 typedef long long ll;
 
-<<<<<<< HEAD
-=======
+const ll break_iteration = 10000000000;
+
 ll y, x, posy, posx, vx, vy;
->>>>>>> 290768e0263d5026839c9405fc99c0085a7040b2
 ll l_a = 1, l_b = 0, d_a = 0, d_b = 1;
 ll gcd = -1;
 
 void extended(ll a, ll b){
 	if(b == 0){
 		gcd = a;
-<<<<<<< HEAD
 		//std::cout << l_a << ' ' << l_b << ' ' << a << std::endl;
-=======
->>>>>>> 290768e0263d5026839c9405fc99c0085a7040b2
 		return;
 	}
 
@@ -35,19 +31,6 @@ void extended(ll a, ll b){
 	extended(b, a % b);
 }
 
-<<<<<<< HEAD
-int main(){
-	std::ios::sync_with_stdio(false);
-
-	ll y, x, posy, posx, vx, vy;
-	std::cin >> x >> y >> posx >> posy >> vx >> vy;
-
-	if(vy == 0 and vx == 0){
-		std::cout << -1 << '\n';
-		return 0;
-	}
-	if(vx == 0){
-=======
 std::vector<ll> solve(ll a1, ll m1, ll a2, ll m2){
 	extended(y, x);
 
@@ -55,7 +38,7 @@ std::vector<ll> solve(ll a1, ll m1, ll a2, ll m2){
 		ll lcm = y / gcd * x;
 
 		ll start = (l_a * a2) % m2 * m1;
-		start   += (l_b * a1) % m1 * m2;
+		start +=   (l_b * a1) % m1 * m2;
 		return {(start + y * x) % (y * x) / gcd, lcm};
 	} else {
 		return {-1, 0};
@@ -66,55 +49,22 @@ void solve_for_0(){
 	if(vy == 0 and vx == 0){
 		std::cout << -1 << '\n';
 	} else if(vx == 0){
->>>>>>> 290768e0263d5026839c9405fc99c0085a7040b2
 		if(posx == x or posx == 0){
 			if(vy == 1){
 				std::cout << posx << ' ' << y << '\n';
 			} else if(vy == -1){
 				std::cout << posx << ' ' << 0 << '\n';
 			}
-<<<<<<< HEAD
-			return 0;
-		} else {
-			std::cout << -1 << '\n';
-			return 0;
-		}
-	}
-	if(vy == 0){
-=======
 		} else {
 			std::cout << -1 << '\n';
 		}
 	} else if(vy == 0){
->>>>>>> 290768e0263d5026839c9405fc99c0085a7040b2
 		if(posy == y or posy == 0){
 			if(vx == 1){
 				std::cout << x << ' ' << posy << '\n';
 			} else if(vx == -1){
 				std::cout << 0 << ' ' << posy << '\n';
 			}
-<<<<<<< HEAD
-			return 0;
-		} else {
-			std::cout << -1 << '\n';
-			return 0;
-		}
-	}
-	ll resy = -1, resx = -1;
-
-	ll alpha1;
-	if(vy == 1){
-		alpha1 = y - posy;
-	} else if(vy == -1){
-		alpha1 = posy;
-	}
-
-	ll alpha2;
-	if(vx == 1){
-		alpha2 = x - posx;
-	} else if(vx == -1){
-		alpha2 = posx;
-=======
 		} else {
 			std::cout << -1 << '\n';
 		}
@@ -139,33 +89,16 @@ int main(){
 	}
 	if(vx == 1){
 		alpha2 = x - posx;
->>>>>>> 290768e0263d5026839c9405fc99c0085a7040b2
 	}
 
 	ll beta1 = y;
 	ll beta2 = x;
 
-<<<<<<< HEAD
-	extended(y, x);
-
-	if(std::abs(alpha1 - alpha2) % gcd == 0){
-		l_a *= (alpha1 - alpha2) / gcd;
-		l_b *= (alpha1 - alpha2) / gcd;
-
-		ll time = alpha1 - beta1 * l_a;
-		if(time < 0){
-			time += y * x;
-		}
-
-		std::vector<ll> updated = {posy + time * vy, posx + time * vx};
-=======
 	ll resy = -1, resx = -1;
 	std::vector<ll> bounce_time = solve(alpha1, beta1, alpha2, beta2);
-
 	if(bounce_time[0] != -1){
 		ll converted = (bounce_time[0] % bounce_time[1] + bounce_time[1]) % bounce_time[1];
 		std::vector<ll> updated = {posy + converted * vy, posx + converted * vx};
->>>>>>> 290768e0263d5026839c9405fc99c0085a7040b2
 
 		if((updated[0] / y) % 2 == 0){
 			resy = 0;
@@ -184,8 +117,4 @@ int main(){
 	} else {
 		std::cout << resx << ' ' << resy << '\n';
 	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 290768e0263d5026839c9405fc99c0085a7040b2
