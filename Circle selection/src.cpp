045b4a6e00df61c17eea644998aa1dcd448dd590
ReaLNeroM <vlade.maksimovski@gmail.com>
2018-05-22 13:26:42 +0200

    Very slight work on Circle Selection

---
+++ Circle selection/src.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+const int maxn = 3e5 + 1e2;
+std::vector<int> adj[maxn];
+int x[maxn], y[maxn], r[maxn];
+
+int main(){
+	std::ios::sync_with_stdio(false);
+
+	int n;
+	std::cin >> n;
+
+	for(int i = 0; i < n; i++){
+		std::cin >> x[i] >> y[i] >> r[i];
+	}
+
+	//fill ez
+}
