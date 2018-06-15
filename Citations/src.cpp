1b030d4950b830678c3b3a58ba12a6e20dff0cda
ReaLNeroM <vlade.maksimovski@gmail.com>
2018-06-16 00:44:29 +0200

    Citations

--- Citations/src.cpp
+++ Citations/src.cpp
@@ -2,58 +2,62 @@
 
 typedef long long ll;
 
-const int maxn = 1e6 + 1e2;
-int length[maxn];
-std::vector<int> adj[maxn];
+const ll maxn = 1e6 + 1e2;
+ll length[maxn];
+std::vector<ll> adj[maxn];
 
-bool cmp(std::pair<int, int> first, std::pair<int, int> second){
-	return first.first * second.second < first.second * second.first;
+bool cmp(std::vector<ll> first, std::vector<ll> second){
+	return first[0] * second[1] < first[1] * second[0];
 }
 
-std::pair<int, int> dfs(int index){
+std::vector<ll> dfs(ll index){
 	if(adj[index].empty()){
-		return {1 + length[index], 1};
+		return {1 + length[index], 1, 1 + length[index]};
 	}
 
-	std::vector<std::pair<int, int>> v(adj[index].size());
+	std::vector<std::vector<ll>> v(adj[index].size());
 
-	for(int i = 0; i < adj[index].size(); i++){
+	for(ll i = 0; i < adj[index].size(); i++){
 		v[i] = dfs(adj[index][i]);
 	}
 
 	std::sort(v.begin(), v.end(), cmp);
 
-	int res = 0;
-	int v_sum_first = 0;
+	ll requisite_sum = 0;
+	ll curr_finish_time = 1;
+	ll sum_v_second = 0;
+	ll sum_v_third = 0;
 
-	for(int i = 0; i < v.size(); i++){
-		res += v_sum_first * v[i].second;
-		v_sum_first += v[i].first;
+	for(ll i = 0; i < v.size(); i++){
+		requisite_sum += curr_finish_time * v[i][1];
+
+		curr_finish_time += v[i][0];
+		sum_v_second += v[i][1];
+		sum_v_third += v[i][2];
 	}
 
-	res += v_sum_first;
-
-	return {1 + res + length[index], v.size() + 1};
+	curr_finish_time += length[index];
+	return {curr_finish_time, (ll) sum_v_second + 1, requisite_sum + curr_finish_time + sum_v_third};
 }
 
 int main(){
 	std::ios::sync_with_stdio(false);
 
-	int n;
+	ll n;
 	std::cin >> n;
 
-	for(int i = 0; i < n; i++){
+	for(ll i = 0; i < n; i++){
 		std::cin >> length[i];
 
-		int requisites;
+		ll requisites;
 		std::cin >> requisites;
 		adj[i].resize(requisites);
 
-		for(int j = 0; j < requisites; j++){
+		for(ll j = 0; j < requisites; j++){
 			std::cin >> adj[i][j];
 			adj[i][j]--;
 		}
 	}
 
-	std::cout << dfs(0).first << std::endl;
+	std::cout << dfs(0)[2] << std::endl;
 }
