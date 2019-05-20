f71d246a2aa7be59df1185fafad640c5ff511aa1
vlade <vlade.maksimovski@gmail.com>
2019-05-20 00:14:20 -0400

    CodeJam

--- Google CodeJam/src.cpp
+++ Google CodeJam/src.cpp
@@ -2,9 +2,8 @@
 
 typedef long long ll;
 
-const ll maxn = 1e2 + 10;
-const ll maxbit = (1 << 17);
-const ll INF = 1000000000000000010;
+const ll maxn = 1e3 + 10;
+std::pair<ll, ll> v[maxn];
 
 int main(){
 	std::ios::sync_with_stdio(false);
@@ -13,8 +12,71 @@ int main(){
 	std::cin >> testCases;
 
 	for(ll testcase = 1; testcase <= testCases; testcase++){
+		ll n;
+		std::cin >> n;
 
-		std::cout << "Case #" << testcase << ": ";
+		for(ll i = 0; i < n; i++){
+			ll a, b;
+			std::cin >> a >> b;
+			v[i] = {a, b};
+		}
+
+		std::sort(v, v + n);
+		
+		ll res = 0;
+		do {
+			long double l = 0.0; // s / f min
+			long double r = 1e9; // s / f  max
+
+			for(ll i = 0; i < n - 1; i++){
+				std::pair<ll, ll> a = v[i];
+				std::pair<ll, ll> b = v[i + 1];
+
+				b.first -= a.first;
+				b.second -= a.second;
+
+				if(b.first <= 0 and b.second <= 0){
+					l = 1.0, r = 0.0;
+					break;
+				}
+				if(b.first >= 0 and b.second >= 0){
+					continue;
+				} else if (b.first > 0 and b.second < 0){
+//					l = std::max(l, (long double) b.first / (long double) (-b.second));
+					 r = std::min(r, (long double) b.first / (long double) (-b.second));
+				} else if(b.first < 0 and b.second > 0){
+					 l = std::max(l, (long double) b.first / (long double) (-b.second));
+//					r = std::min(r, (long double) b.first / (long double) (-b.second));
+				}
+			}
+
+			bool good = true;
+			long double w = (l + r) / 2;
+
+			if(l - 0.0000000000001L > r){
+				good = false;
+			}
+
+			for(ll i = 0; i < n - 1; i++){
+				std::pair<ll, ll> a = v[i];
+				std::pair<ll, ll> b = v[i + 1];
+
+				b.first -= a.first;
+				b.second -= a.second;
+
+				if(b.first + w * b.second <= 0.0000000000001L){
+					good = false;
+				}
+			}
+			
+			if(!good){
+				continue;
+			}
+
+			res++;
+		} while(std::next_permutation(v, v + n));
+
+		std::cout << "Case #" << testcase << ": " << res;
 
 		std::cout << '\n';
 	}
---
+++ Google CodeJam/template.py
@@ -0,0 +1,11 @@
+import sys
+
+test_cases = int(input())
+
+for test_case in range(1, test_cases + 1):
+    for j in range(20):
+        day = int(input())
+        print j + 1, 100
+        sys.stdout.flush()
+
+    # print "Case #" + str(test_case) + ": IMPOSSIBLE"
