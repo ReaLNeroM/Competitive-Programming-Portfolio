986a48c168fc8d379813efda0c24bccc382ff9e8
vlade <vlade.maksimovski@gmail.com>
2019-05-20 00:20:40 -0400

    Added template files for CodeJam

---
+++ Google CodeJam/template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+const ll maxn = 1e6 + 10;
+const ll INF = 1000000000000000010;
+
+int val[maxn];
+
+int main(){
+	std::ios::sync_with_stdio(false);
+
+	ll testCases;
+	std::cin >> testCases;
+
+	for(ll testcase = 1; testcase <= testCases; testcase++){
+		std::cout << "Case #" << testcase << ": ";
+
+		int n;
+		std::cin >> n;
+
+		for(int i = 0; i < n; i++){
+			std::cin >> val[i];
+		}
+
+		//TODO
+
+		std::cout << '\n';
+	}
+}
--- Google CodeJam/template.py
+++ Google CodeJam/template.py
@@ -3,9 +3,10 @@ import sys
 test_cases = int(input())
 
 for test_case in range(1, test_cases + 1):
-    for j in range(20):
-        day = int(input())
-        print j + 1, 100
-        sys.stdout.flush()
+	n = int(input())
 
-    # print "Case #" + str(test_case) + ": IMPOSSIBLE"
+	v = map(int, raw_input.split(' '))
+	
+    print "Case #" + str(test_case) + ": "
+
+    # print stuff
